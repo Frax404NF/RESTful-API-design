22ab57f127930609830a257b9931f5917327f148
Frandi <frandicollage21@gmail.com>
2026-03-30 14:34:05 +0800

    feat: add auth controllers with register, login, and JWT token generation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express'
+import { login, register } from '../controller/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { insertUserSchema } from '../db/schema.ts'
+import { z } from 'zod'
+
+const loginSchema = z.object({
+  email: z.email('Invalid email'),
+  password: z.string().min(1, 'Password is required'),
+})
 
 const router = Router()
 
 // Authentication routes
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'User Registered' })
-})
+router.post('/register', validateBody(insertUserSchema), register)
 
-router.post('/login', (req, res) => {
-  res.json({ message: 'User logged in' })
-})
+router.post('/login', validateBody(loginSchema), login)
 
 router.post('/logout', (req, res) => {
   res.json({ message: 'User logged out' })
